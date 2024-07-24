1c351233a57dfb75c9df59a9a82c699754dc7b7d
PrachiBhari <121476765+PrachiBhari@users.noreply.github.com>
2024-07-24 10:52:57 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-   'https://blogging-backend-j5o3.onrender.com'
+   'https://blogging-backend-j5o3.onrender.com',
+    'https://blogging-frontend.netlify.app'
 
 ];
 
